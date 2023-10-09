7c420de60170970c4f30a225c8fffb51bfb93561
susie <chenpitools@gmail.com>
2023-10-09 11:29:59 +0800

    updated matcher

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RouterModule, UrlSegment } from '@angular/router'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        matcher: (url) => {
+          if (url.length === 1 && url[0].path.match(/^@[\w]+$/gm)) {
+            return {
+              consumed: url,
+              posParams: {
+                username: new UrlSegment(url[0].path.slice(1), {})
+              }
+            };
+          }
+
+          return null;
+        },
+        component: ProfileComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
